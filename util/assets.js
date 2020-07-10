49da8307bfc8ded263fd9955dfaf96c557cc653a
Zacaria <havesomecode@gmail.com>
2020-07-10 21:08:31 +0200

    [ADD] module 7. Delivering content

--- util/assets.js
+++ util/assets.js
@@ -1,4 +1,4 @@
-const base = 'https://hamster-bucket-zacaria-chtatar.s3.amazonaws.com'
+const base = 'https://dqsjkhdksqjh.cloudfront.net'
 module.exports = {
   hamster1: base + '/images/hamster-1-final.png',
   hamster2: base + '/images/hamster-2-final.png',
